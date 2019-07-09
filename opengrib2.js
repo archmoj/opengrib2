b31bf6d4f9e5e00f2de78a94e0747b3489c4988a
archmoj <arch.mojtaba.samimi@gmail.com>
2019-07-08 21:37:18 -0400

    add dummy imports for the moment

--- opengrib2.js
+++ opengrib2.js
@@ -1,3 +1,6 @@
+/* import */ function /* Buffer */ RandomAccessFile(b, c) {};
+/* import */ function /* Buffer */ Grib2JpegDecoder(args) {};
+
 var /* boolean */ log = false;
 
 var asciiTable = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US"];
