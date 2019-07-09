24cf4211c73a59eae34fc0008c9d0ce5abf1759c
archmoj <arch.mojtaba.samimi@gmail.com>
2019-07-09 19:55:07 -0400

    expose DataValues

--- browser.js
+++ browser.js
@@ -32,7 +32,9 @@ var DATA = {
         Filename: "prototype"
 };
 
-var link = 'https://dd.weather.gc.ca/model_gem_global/25km/grib2/lat_lon/00/003/CMC_glb_TMP_ISBL_1000_latlon.24x.24_2019070900_P003.grib2'
+//var link = 'https://dd.weather.gc.ca/model_gem_global/25km/grib2/lat_lon/00/003/CMC_glb_TMP_ISBL_1000_latlon.24x.24_2019070900_P003.grib2'
+var link = 'https://dd.weather.gc.ca/model_hrdps/west/grib2/12/006/CMC_hrdps_west_TMP_TGL_2_ps2.5km_2019070912_P006-00.grib2'
+
 link = link.replace('https://dd.weather.gc.ca/', 'http://localhost:3000/')
 DATA.numMembers = 1; // i.e. deterministic
 
@@ -45,6 +47,40 @@ var myGrid = new GRIB2CLASS(DATA, {
         OutputFolder: OutputFolder
 });
 
+function plot(grid) {
+        var i, v;
+        var canvas = document.getElementById('canvas')
+        var ctx = canvas.getContext('2d')
+        var nx = grid.Nx;
+        var ny  = grid.Ny;
+        var nPoints = nx * ny;
+        canvas.width = nx;
+        canvas.height = ny;
+
+        var values = grid.DataValues[0];
+
+        var min = Infinity;
+        var max = -Infinity;
+        for (i = 0; i < nPoints; i++) {
+                v = values[i];
+                if(min > v) min = v;
+                if(max < v) max = v;
+        }
+
+        var img = ctx.createImageData(nx, ny);
+
+        var q = 0;
+        for (i = 0; i < nPoints; i++) {
+                v = values[i];
+                var c = 255 * (v - min) / (max - min);
+                img.data[q++] = c;
+                img.data[q++] = c;
+                img.data[q++] = c;
+                img.data[q++] = 255;
+        }
+        ctx.putImageData(img, 0, 0);
+}
+
 http.get(link, function (res) {
         var allChunks = [];
         res.on("data", function (chunk) {
@@ -52,5 +88,6 @@ http.get(link, function (res) {
         });
         res.on("end", function () {
                 myGrid.parse(Buffer.concat(allChunks));
+                plot(myGrid);
         });
 });
